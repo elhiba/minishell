78411ca16053a908275d1bdb871d63e359064696
Mohamed Amine El Hiba <moel-hib@c2r4p10.1337.ma>
2025-04-04 23:45:09 +0000

    updates

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,28 +6,25 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 22:16:17 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/03/31 19:45:42 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/04/04 21:42:49 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-/* standard input / output */
-#include <stdio.h>
-
-/* For shit realine lib */
-#include <readline/readline.h>
-
-/* Malloc and free */
-#include <stdlib.h>
-
-/* Handling signals */
-#include <signal.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <unistd.h>
 
 /* Signal func */
 void	signal_init(void);
 void	signal_handler(int sig);
+
+/* Parse */
+void	ft_readline(void);
 
 /* error handler */
 void	error_handler(char *error_name);
