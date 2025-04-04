8165485ecfc39a255a9ce9bacb9d6991e7e3f672
elhiba <elhibamohamedamine@gmail.com>
2025-04-04 10:54:45 +0000

    simple test

--- src/main.c
+++ src/main.c
@@ -6,22 +6,30 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 20:59:49 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/03/28 21:53:59 by elhiba           ###   ########.fr       */
+/*   Updated: 2025/04/02 22:15:12 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <stdlib.h>
+#include "../includes/minishell.h"
 
 int main(void)
 {
-	char	*readline_in;
-	readline_in = readline("$>");
+	static char	*readline_in;
 
-	printf("%s\n", readline_in);
+	signal_init();
 
-	free(readline_in);
+	while (1)
+	{
+		readline_in = readline("$>");
+		if (readline_in == NULL)
+		{
+			printf("exit\n");
+			exit(EXIT_SUCCESS);
+		}
+		printf("%s\n", readline_in);
+		free(readline_in);
+		
+	}
 
 	return (0);
 }
