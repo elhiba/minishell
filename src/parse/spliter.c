16625696dd4e37b5c40c22d79c7c09450a1cf2e8
Mohamed Amine El Hiba <moel-hib@c2r7p10.1337.ma>
2025-05-02 23:42:29 +0100

    Update - Updates readline! - Spliter now splite '| ' ' < << >> >'

--- src/parse/spliter.c
+++ src/parse/spliter.c
@@ -6,33 +6,80 @@
 /*   By: sel-maaq <sel-maaq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 15:42:44 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/04/23 18:32:26 by sel-maaq         ###   ########.fr       */
+/*   Updated: 2025/05/02 23:11:53 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char	*ft_remove_tabs(char *args)
+int		arg_counter(char *str)
 {
 	int	i;
+	int	count;
 
 	i = 0;
-	while (args[i])
+	count = 0;
+	while (str[i])
 	{
-		if (args[i] == '\t' || args[i] == '\n')
-			args[i] = ' ';
+		if ((str[i] == '>' && str[i + 1] == '>')
+			|| (str[i] == '<' && str[i + 1] == '<')
+			|| str[i] == '|'
+			|| str[i] == ' '
+			|| str[i] == '<'
+			|| str[i] == '>')
+			count++;
+		if ((str[i] == '>' && str[i + 1] == '>')
+			|| (str[i] == '<' && str[i + 1] == '<'))
+			i++;
 		i++;
 	}
-	return (args);
+	return (count);
 }
 
-char	**ft_spliter(char *args)
+int		operation_len(char *str)
 {
-	char	**split;
+	if (str[0] == '>' && str[1] == '>')
+		return (2);
+	if (str[0] == '<' && str[1] == '<')
+		return (2);
+	if (str[0] == '|' || str[0] == '<' || str[0] == '>')
+		return (1);
+	return (0);
+}
 
-	ft_remove_tabs(args);
-	split = ft_split(args, ' ');
-	if (!split)
-		error_handler("Split", NULL);
-	return (split);
+char	**ft_spliter(t_data *data)
+{
+	char	**args;
+	int		op_len;
+	int		index;
+	int		i;
+	int		start;
+
+	i = 0;
+	index = 0;
+	args = (char **)malloc(sizeof(char *) * (arg_counter(data->readline_in) + 3));
+	while (data->readline_in[i])
+	{
+		if (data->readline_in[i] == ' ')
+			i++;
+		if (!data->readline_in[i])
+			break ;
+		op_len = operation_len(data->readline_in + i);
+		if (op_len)
+		{
+			args[index] = ft_substr(data->readline_in, i, op_len);
+			index++;
+			i += op_len;
+		}
+		else
+		{
+			start = i;
+			while (data->readline_in[i] && !(data->readline_in[i] == ' ') && !operation_len(data->readline_in + i))
+				i++;
+			args[index] = ft_substr(data->readline_in, start, i - start);
+			index++;
+		}
+	}
+	args[index] = NULL;
+	return (args);
 }
