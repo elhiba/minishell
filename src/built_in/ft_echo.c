30b1bd31dfda716510d42a8b3da561a5b74b3770
elhiba <elhibamohamedamine@gmail.com>
2025-06-27 22:59:52 +0100

    echo + i didn't protect empty echo in input

--- src/built_in/ft_echo.c
+++ src/built_in/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:22:38 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/06/27 22:47:49 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/06/27 22:58:54 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,21 @@ int	do_echo(t_cmd *data)
 	fd = 1;
 	is_newline = 1;
 	ptr = ++data->argv;
-	while (ptr[i][0] == '-' && ptr[i][1] == 'n')
-	{
-		j = 2;
-		while (ptr[i][j] == 'n')
-			j++;
-		if (ptr[i][j] == '\0')
-		{
-			i++;
-			is_newline = 0;
-		}
-		else
-			break ;
-	}
 	if (*ptr)
 	{
+		while (ptr[i][0] == '-' && ptr[i][1] == 'n')
+		{
+			j = 2;
+			while (ptr[i][j] == 'n')
+				j++;
+			if (ptr[i][j] == '\0')
+			{
+				i++;
+				is_newline = 0;
+			}
+			else
+			break ;
+		}
 		while (ptr[i])
 		{
 			write(fd, ptr[i], ft_strlen(ptr[i]));
