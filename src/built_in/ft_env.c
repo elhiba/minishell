a3ffc0789c22bda8a8461547242a7b8982bc34b7
elhiba <elhibamohamedamine@gmail.com>
2025-07-30 14:23:52 +0100

    update env
    + remove '\' character?

--- src/built_in/ft_env.c
+++ src/built_in/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: moel-hib <moel-hib@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 18:46:17 by moel-hib          #+#    #+#             */
-/*   Updated: 2025/07/21 17:31:10 by moel-hib         ###   ########.fr       */
+/*   Updated: 2025/07/30 13:56:39 by moel-hib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ int	do_env(t_cmd *data)
 	{
 		if (ft_strchr(data->data->env[i], '='))
 		{
-			write(fd, ft_strjoin(data->data->env[i], "\n"), ft_strlen(data->data->env[i]));
+			write(fd, ft_strjoin(data->data->env[i], "\n"),
+				ft_strlen(data->data->env[i]));
 		}
 		i++;
 	}
